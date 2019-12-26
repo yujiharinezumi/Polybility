5f51353f8bf542a00961b0a13ed253d6ec2f5be8
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-12-26 16:58:09 +0900

    modified devise setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,8 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :icon, :age, :gender, :country, :native_language, :learning_language, :hobby])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :icon])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :icon, :age, :gender, :country, :native_language, :learning_language, :hobby])
     end
 
     def after_sign_in_path_for(resource)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,13 @@ class UsersController < ApplicationController
   end
 
   def show
-     @user = User.find(params[:id]) 
+     @user = User.find(params[:id])
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :icon, :age, :gender, :country, :native_language, :learning_language, :hobby)
   end
 
   def set_user
