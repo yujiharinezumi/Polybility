af70c35c25a0dc7087c478e1bc663ea351ab3404
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-12-26 15:30:35 +0900

    created country and language models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_26_061003) do
+ActiveRecord::Schema.define(version: 2019_12_26_062953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,9 +25,21 @@ ActiveRecord::Schema.define(version: 2019_12_26_061003) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "countries", force: :cascade do |t|
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "languages", force: :cascade do |t|
+    t.string "language"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
