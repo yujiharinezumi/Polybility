539738c1ccb7130f7df3d9807f6c9ea92ca783bf
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-12-28 17:24:01 +0900

    added csv file that contains all countries name

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+
+CSV.foreach('db/csv/country.csv', headers: true) do |row|
+Country.create(
+  country: row['country']
+)
+end
