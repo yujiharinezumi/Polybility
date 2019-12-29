4095703eee01f623cd0c55e30ff16f07e502320c
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-12-29 10:15:54 +0900

    added flash message func

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -63,7 +63,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
    def after_sign_up_path_for(resource)
-     users_path
+     edit_user_registration_url(current_user.id)
    end
 
   # The path used after sign up for inactive accounts.
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def flash_message(message, klass)
+  content_tag(:div, class: "alert alert-#{klass}") do
+    concat content_tag(:button, 'x', class: 'close', data: {dismiss: 'alert'})
+    concat raw(message)
+  end
+end
 end
