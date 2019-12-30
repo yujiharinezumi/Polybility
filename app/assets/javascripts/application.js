f712e13dd3f287742c48b551b22ac9d29de1a525
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-12-30 11:04:42 +0900

    added image upload function using js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,18 @@
 //= require turbolinks
 //= require jquery/dist/jquery.js
 //= require_tree .
+
+$(function() {
+   function readURL(input) {
+       if (input.files && input.files[0]) {
+       var reader = new FileReader();
+       reader.onload = function (e) {
+   $('#img_prev').attr('src', e.target.result);
+       }
+       reader.readAsDataURL(input.files[0]);
+       }
+   }
+   $("#user_img").change(function(){
+       readURL(this);
+   });
+ });
