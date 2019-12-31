ae604341a4cc3538ff095cbcf5678c6232489ddd
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-12-31 20:13:35 +0900

    modified users view and show css

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,9 +17,10 @@ class ImageUploader < CarrierWave::Uploader::Base
    def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  "/images/" + [version_name, "default.png"].compact.join('_')
+  # "/images/" + [version_name, "default.png"].compact.join('_')
 
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+   "default.png"
    end
 
   # Process files as they are uploaded:
