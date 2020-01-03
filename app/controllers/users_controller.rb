0be56b5ce51901f41d42325a0c291a0f81629ed8
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2020-01-04 00:03:19 +0900

    modified comment & message & post/shows view  css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @users = @user.posts
   end
 
   def following
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  validates :content, presence: true
+
+  # def comment_time
+  #   created_at.strftime("%m/%d/%y at %l:%M %p")
+  # end
 end
