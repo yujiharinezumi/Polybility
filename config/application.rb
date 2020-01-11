a368de63506b89656f94e5be1c81cea290bf7da7
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2020-01-11 17:36:52 +0900

    fix comment view

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,10 @@ module Polybility
      g.fixture_replacement :factory_bot, dir: "spec/factories"
    end
 
+   config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+     html_tag
+   end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
