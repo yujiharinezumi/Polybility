42e22583a4ebb516f01363f0dc8957165e479829
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2020-01-16 15:39:17 +0900

    fixed post model spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,18 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  before do
+    @user = FactoryBot.create(:user_first)
+  end
   it "titleが空ならバリデーションが通らない" do
-    post = Post.new(title: '', content: 'hello world', image:"default.jpg")
+    post = Post.new(title: '', content: 'hello world', image:"default.jpg",user_id: @user.id )
+      expect(post).not_to be_valid
+    end
+
+  it "titleが30文字以上ならバリデーションが通らない" do
+    post = Post.new(title: 'a'*31, content: 'hello world', image:"default.jpg",user_id: @user.id )
       expect(post).not_to be_valid
     end
 
   it "contentが空ならバリデーションが通らない" do
-    post = Post.new(title: 'hello', content: '', image:"default.jpg")
+    post = Post.new(title: 'hello', content: '', image:"default.jpg",user_id: @user.id)
     expect(post).not_to be_valid
     end
 
-    # it "title,contentが記載されている場合バリデーションが通る" do
-    #    post = Post.new(title: 'hello', content: 'hellohello', image:"default.jpg")
-    #    expect(post).to be_valid
-    #  end
+  it "contentが30文字以上ならバリデーションが通らない" do
+    post = Post.new(title: 'hello', content: 'a'*76, image:"default.jpg",user_id: @user.id )
+      expect(post).not_to be_valid
+    end
+
+
+  it "title,contentが記載されている場合バリデーションが通る" do
+    post = Post.new(title: 'hello', content: 'hellohello', image:"default.jpg",user_id: @user.id)
+    expect(post).to be_valid
+  end
 end
