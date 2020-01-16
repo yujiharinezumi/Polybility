31e641ac7d73ac440cb815d8999d8ba5843bd4b1
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2020-01-16 13:05:10 +0900

    added seed data using faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,3 +25,26 @@ CSV.foreach('db/csv/hobby.csv', headers: true) do |row|
     hobby: row['hobby']
   )
 end
+
+
+30.times do |user|
+
+   name = Faker::Name.unique.name
+   email = Faker::Internet.email
+   password = "password"
+
+   User.create!(name: name,
+                email: email,
+                password: password,
+                password_confirmation: password,
+                )
+
+ end
+
+# 管理者の作成
+ User.create!(name: "admin_user",
+            email: "admin_user@gmail.com",
+            password: "adminadmin",
+            password_confirmation: "admin@gmail.com",
+            admin: true
+            )
