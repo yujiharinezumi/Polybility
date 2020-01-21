2a70ebb09d364125e078a00a7fe1b3d18ab687a0
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2020-01-21 14:52:01 +0900

    fixed

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ module Polybility
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
     config.i18n.default_locale = :ja
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     config.generators do |g|
      g.test_framework :rspec,
        fixtures: true,
