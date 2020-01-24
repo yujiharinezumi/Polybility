a4b64e662f98c1b36a805ce6a0795aff8c99f2d8
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2020-01-24 13:58:31 +0900

    added user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+   @user1 = FactoryBot.create(:user_first)
+  end
 
   it "nameが空ならバリデーションが通らない" do
     user = User.new(name: '', email: 'test@gmail.com', password: '111111')
@@ -19,6 +22,16 @@ RSpec.describe User, type: :model do
 
   it "emailが形式が違ったらバリデーションが通らない" do
     user = User.new(name: 'テスト', email: 'testtest', password: '111111')
+    expect(user).not_to be_valid
+  end
+
+  it "emailが大文字でもバリデーションが通る" do
+    user = User.new(name: 'テスト', email: 'TEST@gmail.com', password: '111111')
+    expect(user).to be_valid
+  end
+
+  it "emailがすでに登録されている場合バリデーションが通らない" do
+    user = User.new(name: 'テスト', email: 'a@gmail.com', password: '111111')
     expect(user).not_to be_valid
   end
 
