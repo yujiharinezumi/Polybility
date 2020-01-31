4ae9d6f0bac4fd69a53f4774fb767886d71c9482
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2020-01-31 16:29:28 +0900

    re

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Polybility
     config.i18n.default_locale = :ja
     # 言語ファイルを階層ごとに設定するための記述
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-    # アプリケーションが対応している言語のホワイトリスト(ja = 日本語, en = 英語 zh = 中国語)
+    # アプリケーションが対応している言語のホワイトリスト(ja = 日本語, en = 英語 zh = 簡體中文 zc = 繁體中文)
     config.i18n.available_locales = %i(ja en zh zc)
     # 上記の対応言語以外の言語が指定された場合、エラーとするかの設定
     config.i18n.enforce_available_locales = true
